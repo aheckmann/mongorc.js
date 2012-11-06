cc6d48ec9e44f6891e9c2db58a31c1d062be0a03
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-11-06 11:00:17 -0800

    default prompt displays db name

--- .mongorc.js
+++ .mongorc.js
@@ -1,5 +1,11 @@
+// beautify the printed result sets
 DBQuery.prototype._prettyShell = true
 DBQuery.prototype.ugly = function () {
   this._prettyShell = false;
   return this
+}
+
+// always display the db name in the prompt
+prompt = function () {
+  return db + "> ";
 }

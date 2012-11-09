407bfea86e64a0895e408d22fb3f661b680e04d7
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-11-09 08:16:14 -0800

    better prompt
    now displays either mongod, mongos, or replset related info
    
      mongod|database>
      mongos|host:port|database>
      replsetName:ServerStatus|database>

--- .mongorc.js
+++ .mongorc.js
@@ -7,5 +7,42 @@ DBQuery.prototype.ugly = function () {
 
 // always display the db name in the prompt
 prompt = function () {
-  return db + "> ";
+  var res = rs.status();
+  if (!res || res.errmsg) {
+    // not in a replica set
+    var status = db.serverStatus();
+    return status.process + "|" + status.host + "|" + db + "> ";
+  }
+
+  return replsetPrompt();
 }
+
+/**
+ * Creates a prompt string for replSets
+ *
+ * @return {String}
+ */
+
+function replsetPrompt () {
+  var status;
+  var admin = db.getSiblingDB("admin");
+  var info = admin.runCommand({ replSetGetStatus: 1, forShell: 1});
+
+  if (info.ok) {
+    var state = "";
+    // do we need this?
+    info.members.some(function (member) {
+      if (member.self) {
+        state = member.stateStr;
+        return true;
+      }
+    });
+    status = info.set + ":" + (state || info.myState);
+  } else if (info.info && info.info.length < 20) {
+    // < 20 seems like a hack ??
+    status = info.info;
+  }
+
+  return status + "|" + db + "> "
+}
+

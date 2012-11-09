ab9a7d2c80a7da7cd237ef68d6832dd19192d1a2
Aaron Heckmann <aaron.heckmann+github@gmail.com>
2012-11-09 08:19:19 -0800

    docs

--- .mongorc.js
+++ .mongorc.js
@@ -1,11 +1,27 @@
-// beautify the printed result sets
+;(function () {
+
+/**
+ * Make all queries pretty print by default.
+ */
+
 DBQuery.prototype._prettyShell = true
+
+/**
+ * Allow opting into the default ugly print mode.
+ */
+
 DBQuery.prototype.ugly = function () {
   this._prettyShell = false;
   return this
 }
 
-// always display the db name in the prompt
+/**
+ * Override the default prompt to display info related
+ * to type of server we connected to.
+ *
+ * @return {String}
+ */
+
 prompt = function () {
   var res = rs.status();
   if (!res || res.errmsg) {
@@ -46,3 +62,4 @@ function replsetPrompt () {
   return status + "|" + db + "> "
 }
 
+})();
